9c28ba885fa7fd693255fed00f665fba1879c78d
Rafael Figueiredo <rafael3do@hotmail.com>
2022-11-14 19:37:26 -0300

    Created system select char

--- Dungeon 1d6/Assets/Code/Game.cs
+++ Dungeon 1d6/Assets/Code/Game.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject panelGame;
+    public GameObject panelChar;
     void Start()
     {
         if (Global.isload)
         {
             Debug.Log("Loading");
+            panelChar.SetActive(true);
+            panelGame.SetActive(false);
+        }
+        else
+        {
+            panelChar.SetActive(false);
+            panelGame.SetActive(true);
         }
     }
 
