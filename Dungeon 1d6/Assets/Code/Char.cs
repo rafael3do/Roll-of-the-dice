08b30c1289288ea3a4328547fb264278e0e44592
Rafael Figueiredo <rafael3do@hotmail.com>
2022-11-18 22:28:48 -0300

    Update Char.cs

--- Dungeon 1d6/Assets/Code/Char.cs
+++ Dungeon 1d6/Assets/Code/Char.cs
@@ -22,7 +22,6 @@ public class Char : MonoBehaviour
     public Text defenseux;
     public GameObject ImageCharux;
     public GameObject ImgClassux;
-    public GameObject ImageCharux2;
     public GameObject ImgGenderux;
     public static bool start=false;
     public GameObject PanelPlay;
@@ -40,11 +39,24 @@ public class Char : MonoBehaviour
         ImageCharux = ImageChar;
         ImgClassux = ImgClass;
         ImgGenderux = ImgGender;
-        
-        ImageCharux2=Instantiate(ImageChar, ImageCharux.GetComponent<Transform>().localScale, Quaternion.identity);
-        ImageCharux2.transform.parent = PanelPlay.gameObject.transform;
-        ImageCharux2.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 
+        ImageCharux = Instantiate(ImageChar, ImageCharux.GetComponent<Transform>().localScale, Quaternion.identity);
+        ImageCharux.transform.parent = PanelPlay.gameObject.transform;
+        ImageCharux.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        ImageCharux.transform.localPosition = new Vector2(-143.0f, -160.0f);
+        ImageCharux.GetComponent<Button>().enabled = false;
+
+        ImgClassux = Instantiate(ImgClass, ImgClassux.GetComponent<Transform>().localScale, Quaternion.identity);
+        ImgClassux.transform.parent = PanelPlay.gameObject.transform;
+        ImgClassux.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        ImgClassux.transform.localPosition = new Vector2(-142.0f, -204.0f);
+        ImgClassux.GetComponent<Button>().enabled = false;
+
+        ImgGenderux = Instantiate(ImgGenderux, ImgGenderux.GetComponent<Transform>().localScale, Quaternion.identity);
+        ImgGenderux.transform.parent = PanelPlay.gameObject.transform;
+        ImgGenderux.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        ImgGenderux.transform.localPosition = new Vector2(-142.0f, -252.0f);
+        ImgGenderux.GetComponent<Button>().enabled = false;
         start = false;
     }
 
